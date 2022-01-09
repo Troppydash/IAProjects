aec0a84b752932f027ca2483c2a2799b2cc6eaa9
Terry Qi <fortianrui4@gmail.com>
2022-01-09 19:30:26 +1300

    added graphing code

--- Maths/IA/code/src/lib/urls.ts
+++
@@ -1,2 +0,0 @@
-export const STUDENT_HASH_URL = 'https://spider.scotscollege.school.nz/Spider2011/Handlers/Student.asmx/GetStudents_IncludeHub';
-export const STUDENT_ID_URL = 'https://spider.scotscollege.school.nz/Spider2011/Handlers/Curriculum/LiveData/Achievements.asmx/GetStudentAchievements';
